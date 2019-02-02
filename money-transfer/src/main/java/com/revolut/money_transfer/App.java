31f658af9aefd18366941b4dec65baedc45af2b0
Mateusz Gaweł <mg.mateuszgawel@gmail.com>
2019-02-02 21:02:07 +0100

    initial commit - hello world

---
+++ money-transfer/src/main/java/com/revolut/money_transfer/App.java
@@ -0,0 +1,13 @@
+package com.revolut.money_transfer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
